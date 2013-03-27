9530c0f2c03af6e91858d44343b53cdb0f2319d2
Paul Collins <paul.collins@brightscope.com>
2013-03-27 09:29:50 -0700

    Initial setup of nmap

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Install nmap, command line port scanner
+#
+# Examples
+#
+#   include nmap
+
+class nmap {
+  require homebrew
+
+  homebrew::formula{'nmap':
+    before => Package['boxen/brews/nmap']
+  }
+
+  package{'boxen/brews/nmap':
+    ensure => 6.25
+  }
+}
